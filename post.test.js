434f9c765df0931dd5c0148f5028b96780b2dc46
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-09-25 15:39:04 +0200

    route post tested

--- post.test.js
+++ post.test.js
@@ -1,28 +1,34 @@
-const routes = require('./routes/posts.js');
-const app = require('./app'); 
+const app = require('./index'); 
 const request = require('supertest');
+
+const mongoose = require('mongoose');
+
 
 
 describe('POST /create', () => {
     it('should create a new post when valid data is provided', async () => {
-        const res = await request(app)
+        const post = await request(app)
             .post('/create')
-            .json({
+            .send({
                 title: 'My First Post',
                 content: 'This is the content of my first post.'
             });
-        expect(res.statusCode).toEqual(201);
-        expect(res.body).toHaveProperty('_id');
-        expect(res.body.title).toEqual('My First Post');
+        expect(post.statusCode).toEqual(201);
+        expect(post.body).toHaveProperty('_id');
+        expect(post.body.title).toEqual('My First Post');
     });
 
     it('should return 400 if title is missing', async () => {
-        const res = await request(app)
+        const post = await request(app)
             .post('/create')
-            .json({
+            .send({
                 content: 'This is the content of my post without a title.'
             });
-        expect(res.statusCode).toEqual(400);
-        expect(res.body.message).toEqual('Title is required');
+        expect(post.statusCode).toEqual(400);
+        expect(post.body.message).toEqual('Title is required');
     });
+});
+
+afterAll(async () => {
+    await mongoose.connection.close(); // Close the database connection
 });
